523ff93cb7c72c05748422d2f3b888ec9e6bf1e2
Milan Forsthövel <mforstho@student.codam.nl>
2023-04-18 15:28:40 +0200

    Put the example main from the subject into the main

--- ex03/AMateria.hpp
+++ ex03/AMateria.hpp
@@ -6,18 +6,20 @@
 /*   By: mforstho <mforstho@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/04/17 14:18:58 by mforstho      #+#    #+#                 */
-/*   Updated: 2023/04/17 14:35:35 by mforstho      ########   odam.nl         */
+/*   Updated: 2023/04/18 15:25:58 by mforstho      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
+class ICharacter;
 class AMateria
 {
 	protected:
 		std::string type;
 	public:
 		AMateria(std::string const & type);
+		~AMateria();
 		// [...]
 
 		std::string const & getType() const; //Returns the materia type
